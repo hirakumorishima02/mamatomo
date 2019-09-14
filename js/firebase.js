ac8a8ebfda1dda32f156616e5deb394e4ece9c02
hirakumorishima02 <business.ujinchu@gmail.com>
2019-09-14 23:11:41 +0900

    Separate JS file

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDD8od32Lh6qfvNxRxrytEmwtJw5V7PpUw",
+    authDomain: "mamatomo-no-yami.firebaseapp.com",
+    databaseURL: "https://mamatomo-no-yami.firebaseio.com",
+    projectId: "mamatomo-no-yami",
+    storageBucket: "mamatomo-no-yami.appspot.com",
+    messagingSenderId: "753676767600",
+    appId: "1:753676767600:web:edfd2f83c38e321e2f7fb5"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
---
+++ js/main.js
@@ -0,0 +1,38 @@
+
+var database = firebase.database();
+
+async function getDate(i) {
+
+    var tr = document.createElement('tr');
+    tr.id = 'row'+i;
+    await document.getElementById('wrapper').appendChild(tr);
+
+    var td = document.createElement('td');
+    td.id = 'Name'+i;
+    await document.getElementById('row'+i).appendChild(td);
+
+    var td = document.createElement('td');
+    td.id = 'Point'+i;
+    await document.getElementById('row'+i).appendChild(td);
+
+    var td = document.createElement('td');
+    td.id = 'TuhoCount'+i;
+    await document.getElementById('row'+i).appendChild(td);
+
+    var dataRef = database.ref('/'+ i);
+    var dataRefPoint = database.ref('/'+ i +'/Point');
+
+    dataRef.once("value").then(function(snapshot) {
+        document.getElementById("Name"+i).innerHTML = snapshot.child("Name").val();
+    });
+    dataRefPoint.once("value").then(function(snapshot) {
+        document.getElementById("Point"+i).innerHTML = snapshot.child("GoodPoint").val() - snapshot.child("BadPoint").val();
+    });
+    dataRefPoint.once("value").then(function(snapshot) {
+        document.getElementById("TuhoCount"+i).innerHTML = snapshot.child("TuhoCount").val();
+    });
+};
+
+for(var i = 1; i < 3; i++) {
+    getDate(i);
+};
